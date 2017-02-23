3fa7d220985d7709ef6e3912a5055ef301ee0613
Andrey A. Printsev <andrey.printsev@gmail.com>
2017-02-23 17:57:30 +0300

    allowFormAuthenticationForClients and inmemory authentication

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -5,13 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
@@ -58,6 +62,16 @@ public class Application {
 	}
 
 	@Configuration
+	public static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+		@Autowired
+		public void initialize(AuthenticationManagerBuilder builder) throws Exception {
+			builder.inMemoryAuthentication().withUser("admin").password("password").roles("USER");
+		}
+
+	}
+
+	@Configuration
 	@EnableAuthorizationServer
 	protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
@@ -73,23 +87,33 @@ public class Application {
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 			// @formatter:off
-			clients.inMemory().withClient("my-trusted-client")
+			clients.inMemory()
+					// Client 1
+					.withClient("my-trusted-client")
 					.authorizedGrantTypes("password", "authorization_code",
 							"refresh_token", "implicit")
 					.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
 					.scopes("read", "write", "trust").resourceIds("sparklr")
 					.accessTokenValiditySeconds(60).and()
+
+					// Client 2
 					.withClient("my-client-with-registered-redirect")
 					.authorizedGrantTypes("authorization_code").authorities("ROLE_CLIENT")
 					.scopes("read", "trust").resourceIds("sparklr")
 					.redirectUris("http://anywhere?key=value").and()
+
+					// Client 3
 					.withClient("my-client-with-secret")
 					.authorizedGrantTypes("client_credentials", "password")
 					.authorities("ROLE_CLIENT").scopes("read").resourceIds("sparklr")
-					.secret("secret");
+					;
 			// @formatter:on
 		}
 
+		@Override
+		public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+			security.allowFormAuthenticationForClients();
+		}
 	}
 
 }
